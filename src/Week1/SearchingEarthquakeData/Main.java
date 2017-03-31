8e71286b45d004bcf69aa0f3e6c7863033fbba8a
alexrezv <ramspb@niuitmo.ru>
2017-03-31 16:28:21 +0300

    Done first assignment for the week 1.

---
+++ src/Week1/SearchingEarthquakeData/Main.java
@@ -0,0 +1,10 @@
+package Week1.SearchingEarthquakeData;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hi");
+        EarthQuakeClient eQC = new EarthQuakeClient();
+        //eQC.bigQuakes();
+        eQC.closeToMe();
+    }
+}
