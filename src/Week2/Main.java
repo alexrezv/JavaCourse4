1f3e5eee252bdf9888d20a1a0438ed0eaed036fc
alexrezv <ramspb@niuitmo.ru>
2017-04-09 22:13:22 +0300

    Assignment 2: Bubble Sort for the Week2 is DONE.

--- src/Week2/Main.java
+++ src/Week2/Main.java
@@ -6,7 +6,7 @@ import Week2.ImplementingSelectionSort.QuakeSortInPlace;
  * Created by alex on 05.04.17.
  */
 public class Main {
-    public static void main (String[] args) {
+    public static void main(String[] args) {
         System.out.println("Week2");
         QuakeSortInPlace qsip = new QuakeSortInPlace();
         qsip.testSort();
