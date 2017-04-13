f69892389c0022e23de2d4ebec21ff7582eaea52
alexrezv <ramspb@niuitmo.ru>
2017-04-13 21:10:22 +0300

    Initial commit for the Week 3

---
+++ src/Week3/GeneratingRandomText/Main.java
@@ -0,0 +1,16 @@
+package Week3.GeneratingRandomText;
+
+/**
+ * Created by alex on 13.04.17.
+ */
+public class Main {
+    public static void main(String[] args) {
+        /*
+        * Create MarkovZero generated texts by running the method runMarkovZero in MarkovRunner.
+        * Run the program twice and note that the output is different each time you run it.
+        */
+        MarkovRunner mr = new MarkovRunner();
+        mr.runMarkovZero();
+
+    }
+}
